a8d91be25bee14ad1276b15ca5c88bf6370c73f2
ismiadiiba23-arch <ismiadiiba23@gmail.com>
2026-06-11 06:39:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
+import FSM
+
 import streamlit as st
 
-try:
-    import FSM
-    st.write("FSM berhasil diimport")
-    st.write(dir(FSM))
-except Exception as e:
-    st.error(f"ERROR: {type(e).__name__}: {e}")
+st.write("FSM berhasil diimport")
+st.write(dir(FSM))
+st.stop()
